0da7884dcc4d77ada9fa7b5d92e9b1696d166242
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2022-03-31 09:18:45 +0200

    Scripts update.

--- scripts/bash/installation/system_update.sh
+++ scripts/bash/installation/system_update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# System
 sudo apt update
 sudo apt upgrade
+
+# Snap
 sudo snap refresh
+
+# Conda
 conda update conda
 conda update conda-build
+
+# Python modules
+pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
